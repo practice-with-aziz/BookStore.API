339b8b69ff850c806b3a2b4b89575ae9f81e16c7
Zishan Ansari <rs.azizansari@gmail.com>
2026-04-27 19:02:18 +0530

    feat: add Get all books endpoint

---
+++ BookStore.API/Controllers/BooksController.cs
@@ -0,0 +1,24 @@
+﻿using BookStore.API.Data;
+using BookStore.API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookStore.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+
+    public class BooksController:ControllerBase
+    {
+        private readonly AppDbContext _context;
+        public BooksController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async ActionResult<List<Book>> GetAllBooks()
+        {
+            _context.Books.
+        }
+    }
+}
