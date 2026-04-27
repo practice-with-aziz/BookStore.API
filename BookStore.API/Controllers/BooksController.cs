bea9355af568054ee78d367f2da063a925e8f3af
Zishan Ansari <rs.azizansari@gmail.com>
2026-04-27 19:24:05 +0530

    feat:add GET book by ID endpoint

--- BookStore.API/Controllers/BooksController.cs
+++ BookStore.API/Controllers/BooksController.cs
@@ -1,13 +1,14 @@
 ﻿using BookStore.API.Data;
 using BookStore.API.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace BookStore.API.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
 
-    public class BooksController:ControllerBase
+    public class BooksController : ControllerBase
     {
         private readonly AppDbContext _context;
         public BooksController(AppDbContext context)
@@ -16,9 +17,25 @@ namespace BookStore.API.Controllers
         }
 
         [HttpGet]
-        public async ActionResult<List<Book>> GetAllBooks()
+        public async Task<ActionResult<List<Book>>> GetAllBooks()
         {
-            _context.Books.
+            var allBooks = await _context.Books.ToListAsync();
+            return Ok(allBooks);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Book>> GetBookById(int id)
+        {
+            var book = await _context.Books.FindAsync(id);
+
+            if(book is null)
+            {
+                return NotFound($"Book with ID {id} was not found");
+            }
+            else
+            {
+                return Ok(book);
+            }
         }
     }
 }
