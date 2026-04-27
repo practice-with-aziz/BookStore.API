cf61615fedd9d79c339fdf900265a9b64b2e1b51
Zishan Ansari <rs.azizansari@gmail.com>
2026-04-27 13:44:14 +0530

    feat: add AppDbContext, register DI, apply initial migration

---
+++ BookStore.API/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using BookStore.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookStore.API.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Book> Books { get; set; }
+    }
+}
--- BookStore.API/Program.cs
+++ BookStore.API/Program.cs
@@ -1,10 +1,15 @@
+using BookStore.API.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+        options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
