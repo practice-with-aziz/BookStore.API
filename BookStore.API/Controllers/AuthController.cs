c695c2bdf94cec75b84657d9174f2e23c11cf8e5
Zishan Ansari <rs.azizansari@gmail.com>
2026-04-30 14:48:48 +0530

    feat: add RegisterDto and Register endpoint with BCrypt password hashing

---
+++ BookStore.API/Controllers/AuthController.cs
@@ -0,0 +1,43 @@
+﻿using BookStore.API.Data;
+using BookStore.API.DTOs;
+using BookStore.API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookStore.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+        private readonly IConfiguration _config;
+        public AuthController(AppDbContext context, IConfiguration config)
+        {
+            _context = context;
+            _config = config;
+        }
+
+        [HttpPost("register")]
+        public IActionResult Register(RegisterDto register)
+        {
+            var user = _context.Users.FirstOrDefault(u => u.UserName == register.UserName);
+            if (user is not null)
+            {
+                return BadRequest("User Name already taken, try another user name");
+            }
+
+            var hashedPassword = BCrypt.Net.BCrypt.HashPassword(register.Password);
+
+            var newUser = new User
+            {
+                UserName = register.UserName,
+                PasswordHash = hashedPassword,
+                Role = register.Role
+            };
+
+            _context.Users.Add(newUser);
+            _context.SaveChangesAsync();
+            return Ok("Registration successful");
+        }
+    }
+}
--- BookStore.API/Program.cs
+++ BookStore.API/Program.cs
@@ -1,5 +1,8 @@
 using BookStore.API.Data;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,6 +14,30 @@ builder.Services.AddOpenApi();
 builder.Services.AddDbContext<AppDbContext>(options =>
         options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+var jwtSettings = builder.Configuration.GetSection("JwtSettings");
+var secretKey = jwtSettings["SecretKey"];
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = jwtSettings["Issuer"],
+        ValidAudience = jwtSettings["Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secretKey!))
+    };
+});
+
+builder.Services.AddAuthorization();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -20,6 +47,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
